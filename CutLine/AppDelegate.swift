bb38026ebc8b0f2b1fb5f25ff184e6dc1f7ef364
Martin Jacala <martin.jacala@gmail.com>
2018-10-12 19:46:10 +0200

    Initial Commit

---
+++ CutLine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CutLine
+//
+//  Created by Martin Jacala on 12/10/2018.
+//  Copyright © 2018 Martin Jacala. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
